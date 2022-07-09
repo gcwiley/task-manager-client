7c4cbbe5297fa557286cc9b55a9eb5ac947bc26d
Greg Wiley <gcwiley@gmail.com>
2022-07-09 11:33:19 -0700

    additional code added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
